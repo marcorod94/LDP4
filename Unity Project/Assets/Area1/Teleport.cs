b684635d2beb08f3d3d531bdaf2e1de92aaa4560
Ana Hernández <anaisabel14hb@gmail.com>
2020-01-26 18:46:54 +0100

    Add teleport script and prefab

---
+++ Unity Project/Assets/Area1/Teleport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public Vector3 destination;
+    
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Teleport");
+        collider.gameObject.transform.position = destination;
+    }
+}
