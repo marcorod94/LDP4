8b1bd508f99034593a949c30577fbaf993aec38d
Dartemiss <enrique.alexand.gonzalez@est.fib.upc.edu>
2020-02-07 10:37:34 +0100

    Added cameas

---
+++ Unity Project/Assets/Scripts/TriggerCameraChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Elenesski.Camera.Utilities;
+
+public class TriggerCameraChange : MonoBehaviour
+{
+
+    public GenericMoveCamera camera;
+    public GameObject alternativeLookAt;
+
+     [SerializeField]
+     private float offset = 10.0f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        camera.LookAtTarget = alternativeLookAt;
+        
+    }
+
+}
